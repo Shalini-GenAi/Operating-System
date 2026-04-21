491f176b72ced7ac1add01f4acd02b8b593907a9
Shalini Negi <shalininegi196@gmail.com>
2026-04-21 20:58:16 +0530

    Add First Fit memory allocation algorithm
    Implements the First Fit memory allocation algorithm to allocate memory blocks to processes based on their sizes.

---
+++ FirstFit.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void firstFit(int blockSize[], int m, int processSize[], int n) {
+    int allocation[n]; // Stores block index for each process
+    for (int i = 0; i < n; i++)
+        allocation[i] = -1; // Initially no allocation
+
+    // For each process
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (blockSize[j] >= processSize[i]) {
+                // Allocate block j to process i
+                allocation[i] = j;
+                blockSize[j] -= processSize[i];
+                break; // Stop after first fit
+            }
+        }
+    }
+
+    // Print results
+    printf("\nProcess No.\tProcess Size\tBlock No.\n");
+    for (int i = 0; i < n; i++) {
+        if (allocation[i] != -1)
+            printf("%d\t\t%d\t\t%d\n", i + 1, processSize[i], allocation[i] + 1);
+        else
+            printf("%d\t\t%d\t\tNot Allocated\n", i + 1, processSize[i]);
+    }
+}
+
+int main() {
+    int blockSize[] = {100, 500, 200, 300, 600}; // Memory blocks
+    int processSize[] = {212, 417, 112, 426};    // Processes
+    int m = sizeof(blockSize) / sizeof(blockSize[0]);
+    int n = sizeof(processSize) / sizeof(processSize[0]);
+
+    printf("Memory Blocks: ");
+    for (int i = 0; i < m; i++) printf("%d ", blockSize[i]);
+    printf("\nProcesses: ");
+    for (int i = 0; i < n; i++) printf("%d ", processSize[i]);
+
+    firstFit(blockSize, m, processSize, n);
+
+    return 0;
+}
